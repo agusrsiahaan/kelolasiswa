c04d8f01f37db9d76232091049694beeae8437e2
Agus R Siahaan <agusronaldo2@gmail.com>
2020-04-29 05:15:31 -0700

    search function

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -8,9 +8,13 @@ use App\Siswa;
 
 class SiswaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-    	$siswa = Siswa::all();
+    	if ($request->has('cari')) {
+    		$siswa = Siswa::where('nama_depan', 'LIKE', '%'.$request->cari.'%')->get();
+    	}else{
+    		$siswa = Siswa::all();
+    	}
     	return view('siswa.index', [
     		'siswa' => $siswa
     	]);
