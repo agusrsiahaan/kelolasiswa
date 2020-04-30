a659917aed0a6f455b7f4a0e9e60e0eaac064f63
Agus R Siahaan <agusronaldo2@gmail.com>
2020-04-29 20:33:47 -0700

    autentikasi function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -9,5 +9,19 @@ class AuthController extends Controller
     public function login()
     {
     	return view('auths.login');
+    }
+
+    public function postlogin(Request $request)
+    {
+    	if (Auth::attempt($request->only('email', 'password'))) {
+    		return redirect('/dashboard');
+    	}
+    	return redirect('/login');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/login');
     }
 }
